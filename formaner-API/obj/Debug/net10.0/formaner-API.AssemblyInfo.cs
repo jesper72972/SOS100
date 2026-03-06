5ed7e42418241fcf6d90fd968836a5d5b0d6b6bf
jesper72972 <jesper.lindqvist@student.hv.se>
2026-03-06 13:26:56 +0100

    API:et

--- formaner-API/obj/Debug/net10.0/formaner-API.AssemblyInfo.cs
+++ formaner-API/obj/Debug/net10.0/formaner-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("formaner-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5d1d855fb4d9dd9e6da10346a3469ecad4c260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9aad3be8df689236a222b69b96febf1fe77234")]
 [assembly: System.Reflection.AssemblyProductAttribute("formaner-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("formaner-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
