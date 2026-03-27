9d587b4003e3378683b48b52a296afdf8a42d6a8
T-Torq <axel.lindblad@telia.com>
2026-03-27 02:33:05 +0100

    ujkyghvjjgyhgjhy

---
+++ status-formaner-API/Migrations/20260326232718_Initial.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace status_formaner_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StatusOBJ = table.Column<int>(type: "INTEGER", nullable: false),
+                    AdminComment = table.Column<string>(type: "TEXT", nullable: true),
+                    UserCommemt = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceStatus",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    ServicID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceStatus", x => new { x.ID, x.ServicID });
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceStatus_ID",
+                table: "ServiceStatus",
+                column: "ID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "ServiceStatus");
+        }
+    }
+}
