c8c6e38050f1b93e6ae8a010fd0727c46066bda8
Yacqub Elmi <yacqub.katsuro@hotmail.com>
2026-03-28 02:34:05 +0100

    Min Frontend

---
+++ SOS100/Controllers/ApplicationControllers.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Application_API.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace SOS100.Controllers;
+
+public class ApplicationsController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public ApplicationsController(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    // GET: /Applications
+    public async Task<IActionResult> Index()
+    {
+        var data = await _httpClient.GetFromJsonAsync<List<Application>>(
+            "http://localhost:5050/api/applications"
+        );
+
+        return View(data);
+    }
+
+    // GET: /Applications/Create
+    public IActionResult Create(string benefit)
+    {
+        var model = new Application
+        {
+            BenefitName = benefit
+        };
+
+        return View(model);
+    }
+
+    // POST: /Applications/Create
+    [HttpPost]
+    public async Task<IActionResult> Create(Application application)
+    {
+        application.Status = "Pending";
+
+        await _httpClient.PostAsJsonAsync(
+            "http://localhost:5050/api/applications",
+            application
+        );
+
+        return RedirectToAction("Index");
+    }
+}
